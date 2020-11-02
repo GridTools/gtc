591c2a5f986d3d429e8a68f4100a4a0efdc804cd
Hannes Vogt <hannes@havogt.de>
2020-11-02 14:46:57 +0100

    Apply suggestions from code review

--- src/gtc/unstructured/usid_codegen.py
+++ src/gtc/unstructured/usid_codegen.py
@@ -274,7 +274,7 @@ class UsidCodeGenerator(codegen.TemplatedGenerator):
 
 class UsidGpuCodeGenerator(UsidCodeGenerator):
 
-    cache_allocator_ = "gridtools::sid::host_device::make_cached_allocator(&gridtools::cuda_util::cuda_malloc<char[]>);"
+    cache_allocator_ = "gridtools::sid::make_cached_allocator(&gridtools::cuda_util::cuda_malloc<char[]>);"
 
     headers_ = UsidCodeGenerator.headers_ + [
         "<gridtools/next/cuda_util.hpp>",
@@ -329,7 +329,7 @@ class UsidGpuCodeGenerator(UsidCodeGenerator):
 class UsidNaiveCodeGenerator(UsidCodeGenerator):
 
     cache_allocator_ = (
-        "gridtools::sid::host_device::make_cached_allocator(&std::make_unique<char[]>);"
+        "gridtools::sid::make_cached_allocator(&std::make_unique<char[]>);"
     )
 
     KernelCall = as_mako(
